04e219c07673df50033c92eebcdf7dde215390ef
05Przemek <przemochmielowiec@gmail.com>
2023-07-25 13:50:46 +0200

    ...

---
+++ funkcja if/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace funkcja_if
+{
+    class Program
+    {
+        static void Main()
+        {
+            bool pierwszaZmiennaTypuBoolean = false;
+            bool drugaZmiennaTypuBoolean = false;
+            bool trzeciaZmiennaTypuBoolean = false;
+
+            if (pierwszaZmiennaTypuBoolean = true)
+            {
+                Console.WriteLine("Pierwsza zmienna typu Boolean jest warta true");
+            }
+            if (drugaZmiennaTypuBoolean = true)
+            {
+                Console.WriteLine("Druga zmienna typu Boolean jest warta true");
+            }
+            if (trzeciaZmiennaTypuBoolean = true)
+            {
+                Console.WriteLine("Trzecia zmienna typu Boolean jest warta true");
+            }
+            if (pierwszaZmiennaTypuBoolean || drugaZmiennaTypuBoolean) || trzeciaZmiennaTypuBoolean == false)
+            {
+                Console.WriteLine("Nie wiem jak, nie wiem gdzie, ale zepsułeś")
+            }
+        }
+
+    }
+}
